7ee71a79e4f38554bb1d39b0cee19902c03ada91
jazezy <113695364+jazezy@users.noreply.github.com>
2024-08-10 23:59:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -60,7 +60,29 @@ const messages = [
     "Even when things get overwhelming, know that I’m cheering you on every step of the way.",
     "You’re doing an amazing job, and I’m proud of you. I’m here to support you through it all.",
     "No matter how hectic things get, I’m just a call away if you need a break or a smile.",
-    "You’re handling everything so well. I’m here for you, and we’ll get through this together."
+    "You’re handling everything so well. I’m here for you, and we’ll get through this together.",
+    "Hi, hindi ko alam if binubuksan mo ulit to but gusto ko lang sabihin na",
+    "Sobrang grateful ko na nakilala kita and naniniwala ako na it's God's will na makilala ka",
+    "Nasubukan ko na umalis ng comfort zone ko dahil sayo masaya ako na nagawa ko yun kasama ka",
+    "Mag travel kung saan saan kahit hindi natin alam papunta Gino-google maps lang natin",
+    "I wish na makapag travel pa tayo together sa mga malalayong lugar ganon",
+    "Yung tipong maliligaw na tayo, but alam ko na kahit maligaw man tayo magiging masaya lang",
+    "Alam mo ba na ang daya mo, minsan pag nag tatampo mag smile ka lang sakin nawawala tampo ko hmp",
+    "To be honest I miss our late-night talks when we were so sleepy that we'd fall asleep on the call",
+    "Tapos magigising nalang tayo hindi pa napapatay yung call",
+    "But alam ko naman na busy na ngayon hindi na tulad ng dati",
+    "Lagi mo pong tandaan na andito lang ako palagi i can be yung rest po",
+    "I hope I can be the rest you're looking for",
+    "You’re the person I want to share all my dreams with, and I’m so lucky to have you by my side.",
+    "I want to be the one who’s there for you, through thick and thin, the one who makes your days a little brighter.",
+    "Kung nababsa mo man to Jynne gusto sabihin na",
+    "mahal na mahal kita, I wish na hindi tayo mapagod sa isa't isa",
+    "na kahit pagod na pagod na tayo mag sta-stay pa rin tayo kasi pahinga natin ang isa't isa",
+    "Sana hindi ka pa rin mawalan ng gana kausap ako, natatakot na ako",
+    "natatakot ako na baka may magawa ako na tapos mawalan ka ng gana",
+    "kaya lagi ako nag pe-pray kay God na sana i guide nya ako palagi sa mga actions ko",
+    "I look forward to all the dreams we’ve yet to chase together, the places we’ll explore, and the memories we’ll create.",
+    "I Love You"
 ];
 
 let currentIndex = 0;
